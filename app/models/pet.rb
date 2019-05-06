78488be8483b2a565168a8a4bea6528f9e454aa1
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-05-06 20:24:05 +1000

    add select field for category and validate some fields

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :age, numericality: true
+  validates :price, numericality: true
 end
